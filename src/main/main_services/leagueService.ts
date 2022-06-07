c6a660f67a08ec6c047a0755c2778f649b1328f6
Andy Plank <aplank14@gmail.com>
2022-06-06 23:34:33 -0400

    Removed static data

--- src/main/main_services/leagueService.ts
+++ src/main/main_services/leagueService.ts
@@ -3,14 +3,13 @@ import axios from 'axios';
 import LeagueResData from 'renderer/Interfaces/LeagueResData';
 import CurrentEvents from 'renderer/Interfaces/CurrentEvents';
 import processData from './leagueHelper';
-import staticData from './static.json';
 
 export default class LeagueService {
 	url: string = 'https://127.0.0.1:2999/liveclientdata/allgamedata';
 
 	lastUpdate: number = 0;
 
-	async getData(isDebug: boolean): Promise<CurrentEvents | null> {
+	async getData(): Promise<CurrentEvents | null> {
 		const instance = axios.create({
 			httpsAgent: new https.Agent({
 				rejectUnauthorized: false,
@@ -27,8 +26,6 @@ export default class LeagueService {
 			}
 			return null;
 		} catch (err) {
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-			if (isDebug) return processData(staticData as any, this.lastUpdate);
 			return null;
 		}
 	}
