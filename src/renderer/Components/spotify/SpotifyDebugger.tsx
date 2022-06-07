c2225c543b2568c01917f5495a843788c388a7f0
Andy Plank <aplank14@gmail.com>
2022-06-07 01:59:48 -0400

    Removed debug button

--- src/renderer/Components/spotify/SpotifyDebugger.tsx
+++ src/renderer/Components/spotify/SpotifyDebugger.tsx
@@ -1,75 +1,15 @@
-import axios from 'axios';
 import { useSelector } from 'react-redux';
 import MainState from 'renderer/Interfaces/MainState';
-import Playlist from 'renderer/Interfaces/Playlist';
 import JSONPrinter from '../global/JSONPrinter';
 
 const SpotifyAuth = () => {
-	// Searched songs
+	// Spotify auth
 	const spotifyAuth = useSelector((state: MainState) => state.spotifyAuth);
-	const settings = useSelector((state: MainState) => state.settings.value);
-
-	// Get library
-	const library: Playlist[] = useSelector(
-		(state: MainState) => state.library.value
-	);
-
-	// Play songs
-	const playSong = async (playlistId: string) => {
-		const playlist = library.find((e) => e.id === playlistId);
-		if (playlist === undefined) return;
-		const playlistSongs = Object.values(playlist.songs);
-		const songURIs = playlistSongs.map((e) => e.uri);
-		const body = {
-			uris: songURIs,
-			position_ms: 0,
-		};
-		let tokenUrl = 'https://api.spotify.com/v1/me/player/play';
-		if (settings && settings.spotifyDevice) {
-			tokenUrl = `${tokenUrl}?device_id=${settings.spotifyDevice.id}`;
-		}
-		axios({
-			url: tokenUrl,
-			method: 'put',
-			headers: {
-				Authorization: `Bearer ${spotifyAuth.spotifyAccessToken?.authToken}`,
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-			data: body,
-		}).catch(() => {});
-	};
-	const playButtons = library.map((item) => {
-		return (
-			<li
-				key={`${item.id}play`}
-				onClick={() => playSong(item.id)}
-				aria-hidden="true"
-			>
-				<span className="dropdown-item">{item.name}</span>
-			</li>
-		);
-	});
 
 	return (
 		<div>
 			<h1 className="text-center">Spotify Debugger</h1>
 			<div className="container pt-4 pb-2">
-				<h4>Test a playlist</h4>
-				<div className="dropdown">
-					<button
-						className="btn btn-secondary dropdown-toggle"
-						type="button"
-						id="playlistAdd"
-						data-bs-toggle="dropdown"
-						aria-expanded="false"
-					>
-						Shuffle
-					</button>
-					<ul className="dropdown-menu" aria-labelledby="playlistAdd">
-						{playButtons}
-					</ul>
-				</div>
 				<h4>Spotify Auth</h4>
 				<JSONPrinter data={spotifyAuth} />
 			</div>
